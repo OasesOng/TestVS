fcafb1676dc73e5ff4eac7eab33d7c7540a23e6d
OasesOng <fish.0829@outlook.com>
2016-06-24 15:15:21 +0800

    06.24 Test owin register

--- TestAPI/TestAPI/Controllers/AccountController.cs
+++ TestAPI/TestAPI/Controllers/AccountController.cs
@@ -19,7 +19,7 @@ using TestAPI.Results;
 
 namespace TestAPI.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [RoutePrefix("api/Account")]
     public class AccountController : ApiController
     {
